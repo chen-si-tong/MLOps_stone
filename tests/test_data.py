e60ff7aa5aeb3f3d6ba6f6f1627423cecd68e04c
stone <stone.sitong.chen@outlook.com>
2024-01-07 21:09:40 +0100

    edit oath

--- tests/test_data.py
+++ tests/test_data.py
@@ -8,7 +8,7 @@ from tests import _PATH_DATA
 path_data= _PATH_DATA 
 
 def test_data():
-    with open(os.path.join(path_data, 'processed/train_dataset.pkl'), 'rb') as f:
+    with open('processed/train_dataset.pkl', 'rb') as f:
         dataset = pickle.load(f)
     assert dataset[0][0].shape == (1, 28, 28)
     print('test_data passed!')
